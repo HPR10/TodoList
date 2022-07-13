17f106c4bd4f6eca1e25c64db5fff165bb77b898
Hugo Pinheiro <hugo.raimundo@e-deploy.com.br>
2022-07-12 22:30:33 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Hugo Pinheiro on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
